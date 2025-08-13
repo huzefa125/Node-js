bf03bbd85f53dc173eadad5a675487531387bc16
Huzefa <Huzafahuzafa63@gmail.com>
2025-08-13 22:15:24 +0530

    first

--- Node/Server/index.js
+++ Node/Server/index.js
@@ -1,23 +1,44 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
-    const log = `${Date.now()}: ${req.url} New Request Received\n`;
+    if (req.url === '/favicon.ico') return res.end();
+
+    const log = `${Date.now()}: ${req.method} ${req.url} New Request Received\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
 
     fs.appendFile("log.txt", log, (err) => {
         if (err) console.error("Logging failed:", err);
     });
 
-    switch (req.url) {
+    switch (myUrl.pathname) {
         case '/':
             res.end("Home Page");
             break;
+
         case '/about':
-            res.end("About");
+            const username = myUrl.query.myname || "Guest";
+            res.end(`Hi ${username}`);
             break;
+
+        case '/search':
+            const search = myUrl.query.search_query || "error";
+            res.end(`Here are your results for ${search} `);
+            break; // <-- Must have break
+
+        case '/signup':
+            if(req.method === 'GET'){
+                res.end("his is sign up page")
+            }
+            else {
+                res.end("This is normal page")
+            }
+            break
         default:
-            res.end("404 error");
+            res.end("404 Error - Page Not Found");
     }
 });
 
-myServer.listen(8000, () => console.log("Server Started"));
+myServer.listen(8000, () => console.log("Server Started on port 8000"));
